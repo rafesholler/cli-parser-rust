804e62a3f48b30c7eb8e5f076fbab142a2bf1ec4
rafesholler <rafe.sholler@gmail.com>
2025-03-29 15:54:27 -0400

    Improved documentation

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,10 @@
 //!     args.next();
 //!     
 //!     let hashmap = parser.parse(&mut args).unwrap();
+//! 
+//!     if hashmap.contains_key("help") {
+//!         println!("Help argument called!");
+//!     }
 //! }
 //! ```
 
@@ -78,7 +82,18 @@ enum ArgTypes {
     None
 }
 
-/// Represents a single argument for a Parser.
+/// Represents a single argument which can be passed to a [`Parser`].
+/// 
+/// # Example
+/// ```
+/// let arg = Arg::new().param("num");
+/// let parser = Parser::new();
+/// let mut args = std::env::args();
+/// args.next();
+/// 
+/// parser.add_arg(arg);
+/// let output = parser.parse(&mut args).unwrap();
+/// ```
 #[derive(Clone, Debug)]
 pub struct Arg {
     name: String,
@@ -88,6 +103,11 @@ pub struct Arg {
 
 impl Arg {
     /// Create a new arg object, note you must call further methods on this for it to be useful.
+    /// 
+    /// # Example
+    /// ```
+    /// let arg = Arg::new();
+    /// ```
     pub fn new() -> Arg {
         
         Arg { name: String::new(), arg_type: ArgTypes::None, expecting: false} 
@@ -95,21 +115,45 @@ impl Arg {
 
     /// A parameter argument, or one that does not expect any argument to come before it.
     /// Note that the order that these are added to the parser matters.
+    /// 
+    /// # Example
+    /// ```
+    /// let arg = Arg::new().param("p1");
+    /// ```
+    /// `arg` is a required argument and the [`Parser::parse()`] will return an error if it is not present.
     pub fn param(self, name: &str) -> Arg {
         Arg { name: String::from(name), arg_type: ArgTypes::Param(false), expecting: false }
     }
 
-    /// An argument that expects a string of input to follow afterwards.
+    /// An optional argument that expects a value to follow directly after it.
+    /// 
+    /// # Example
+    /// ```
+    /// let arg = Arg::new().input("inp");
+    /// ```
+    /// Upon parsing, if `--inp` is one of the arguments called, `arg` will be in the output with whatever string comes next in the arguments.
     pub fn input(self, name: &str) -> Arg {
         Arg { name: String::from(name), arg_type: ArgTypes::Input, expecting: true }
     }
 
-    /// A flag argument, or one that toggles a setting without excpecting another token afterwards.
+    /// A flag argument, or one that toggles a setting without expecting another token afterwards.
+    /// 
+    /// # Example
+    /// ```
+    /// let arg = Arg::new().flag("optional");
+    /// ```
+    /// Upon parsing, if `--optional` is one of the arguments called, `arg` will be in the output with the value `true`.
     pub fn flag(self, name: &str) -> Arg {
         Arg { name: String::from(name), arg_type: self.arg_type, expecting: false }
     }
 
-    /// A short argument, or one that can be called with a single dash and a character as well as the default way.
+    /// Sets a short option for the argument, allowing it to be called with a char rather than a string.
+    /// 
+    /// # Example
+    /// ```
+    /// let arg = Arg::new().flag("help").short('h');
+    /// ```
+    /// The `arg` variable can be called by `--help` or by `-h`.
     pub fn short(self, ch: char) -> Arg {
         Arg { name: self.name, arg_type: ArgTypes::Short(ch), expecting: self.expecting }
     }
@@ -119,7 +163,18 @@ impl Arg {
     }
 }
 
-/// A struct that parses the command line for certain user-defined args.
+/// A struct that parses the command line for certain [`Arg`]s.
+/// 
+/// # Example
+/// ```
+/// let parser = Parser::new();
+/// let arg = Arg::new().param("num");
+/// let mut args = std::env::args();
+/// args.next();
+/// 
+/// parser.add_arg(arg);
+/// let output = parser.parse(&mut args).unwrap();
+/// ```
 pub struct Parser {
     args: RefCell<Vec<Arg>>,
 }
@@ -127,26 +182,60 @@ pub struct Parser {
 impl Parser {
 
     /// Creates a new Parser struct.
+    /// 
+    /// # Example
+    /// ```
+    /// let parser = Parser::new();
+    /// ```
     pub fn new() -> Parser {
         Parser { args: RefCell::new(vec![]) }
     }
 
     /// Adds an argument to the parser.
+    /// 
+    /// # Example
+    /// ```
+    /// let arg = Arg::new().param("num");
+    /// let parser = Parser::new();
+    /// 
+    /// parser.add_arg(arg);
+    /// ```
     pub fn add_arg(&self, arg: Arg) {
         self.args.borrow_mut().push(arg);
     }
 
     /// Adds a vector of arguments to the parser.
-    pub fn add_args(&self, args: &mut Vec<Arg>) {
-        self.args.borrow_mut().append(args);
+    /// 
+    /// # Example
+    /// ```
+    /// let mut args = vec![
+    ///     Arg::new().param("num1"),
+    ///     Arg::new().param("num2"),
+    /// ];
+    /// let parser = Parser::new();
+    /// 
+    /// parser.add_args(args);
+    /// ```
+    pub fn add_args(&self, mut args: Vec<Arg>) {
+        self.args.borrow_mut().append(&mut args);
     }
 
-
     /// Returns the arguments associated with this parser as a vector.
+    /// 
+    /// # Example
+    /// ```
+    /// let args = parser.args();
+    /// ```
     pub fn args(&self) -> Vec<Arg> {
         self.args.borrow().clone()
     }
 
+    /// Returns the number of arguments associated with this parser.
+    /// 
+    /// # Example
+    /// ```
+    /// let n = parser.len();
+    /// ```
     pub fn len(&self) -> usize {
         self.args.borrow().len()
     }
@@ -156,6 +245,26 @@ impl Parser {
     }
 
     /// Parses through the remaining arguments and returns a hashmap of arguments passed and their relevant values.
+    /// 
+    /// # Example
+    /// ```
+    /// let parser = Parser::new();
+    /// let mut args = vec![
+    ///     Arg::new().param("p1"),
+    ///     Arg::new().param("p2"),
+    ///     Arg::new().flag("help").short('h');
+    /// ];
+    /// parser.add_args(args);
+    /// 
+    /// let mut input_args = std::env::args();
+    /// input_args.next();
+    /// 
+    /// let hashmap = parser.parse(input_args).unwrap();
+    /// println!("p1: {}, p2: {}", hashmap.get("p1"), hashmap.get("p2"));
+    /// if hashmap.contains_key("help") {
+    ///     println!("Help requested!");
+    /// }
+    /// ```
     pub fn parse(&self, args: &mut impl Iterator<Item = String>) -> Result<HashMap<String, Option<String>>, Box<dyn Error>> {
         let mut hashmap: HashMap<String, Option<String>> = HashMap::new();
         let mut prev_arg: Option<Box<Arg>> = None;
@@ -238,7 +347,6 @@ impl Parser {
                         return self.get_err(InvalidCommandReasons::Unexpected(c_arg));
                     }
                 } else {
-                    println!("{:?}", prev_arg);
                     match hashmap.insert(prev_arg.unwrap().name, Some(c_arg.clone())) {
                         Some (_) => return self.get_err(InvalidCommandReasons::Duplicate(c_arg)),
                         None => {}
@@ -319,7 +427,7 @@ mod tests {
         let parser = Parser::new();
         parser.add_arg(default);
         assert_eq!(parser.len(), 1);
-        parser.add_args(&mut vec![short, flag]);
+        parser.add_args(vec![short, flag]);
         assert_eq!(parser.len(), 3);
         assert_eq!(parser.args()[2].name, "flag");
     }
@@ -333,7 +441,7 @@ mod tests {
         let param2 = Arg::new().param("p2");
 
         let parser = Parser::new();
-        parser.add_args(&mut vec![default, short, flag, param1, param2]);
+        parser.add_args(vec![default, short, flag, param1, param2]);
 
         // Tests err on duplicate value
         let mut cmd = "--short short_inp -s s_inp p1 p2"
@@ -394,7 +502,7 @@ mod tests {
         let param2 = Arg::new().param("path");
 
         let parser = Parser::new();
-        parser.add_args(&mut vec![default, short, flag, param1, param2]);
+        parser.add_args(vec![default, short, flag, param1, param2]);
 
         let mut cmd = "--default def_arg filename -s s_arg -f pathname"
             .split_whitespace()
